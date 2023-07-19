3b82c89c11162ebe15843958129db9c15b8b1942
rvdegroen <suiyichiro@hotmail.com>
2023-07-19 15:44:56 +0200

    emitted event

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
-    },
-    methods: {}
-})
+	data() {
+		return {
+			cart: 0,
+			premium: true,
+		};
+	},
+	methods: {
+		updateCart() {
+			this.cart += 1;
+		},
+	},
+});
